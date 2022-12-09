ed9baa926e01c8cf7e97926e5b0a9cb1fc28efb7
notzamarunchik <114111117+notzamarunchik@users.noreply.github.com>
2022-12-09 14:27:05 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,16 +6,20 @@ class testCalculator(unittest.TestCase):
         self.calculator = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calculator.sum(6, 9), 14)
+        self.assertEqual(self.calculator.sum(6, 9), 15)
+        self.assertEqual(self.calculator.sum(6, 91), 97)
 
     def test_subtract(self):
         self.assertEqual(self.calculator.razn(16, 9), 7)
+        self.assertEqual(self.calculator.razn(90, 7), 83)
 
     def test_multiply(self):
         self.assertEqual(self.calculator.proiz(9, 9), 81)
+        self.assertEqual(self.calculator.proiz(6, 8), 48)
 
     def test_devide(self):
         self.assertEqual(self.calculator.deli(24, 8), 3)
+        self.assertEqual(self.calculator.deli(48, 4), 12)
 
 if __name__ == "__main__":
     unittest.main()
